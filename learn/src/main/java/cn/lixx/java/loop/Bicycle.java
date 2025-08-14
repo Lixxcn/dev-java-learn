70917f66f075bc92c7cff412670add512a26dfc1
Lixx <i@lixx.cn>
2025-08-14 17:26:47 +0800

    feat: 初始化Java项目结构并添加示例代码
    添加.gitignore文件忽略.class文件
    创建多个Java示例文件包括基础HelloWorld、Scanner使用等
    添加Maven项目结构和VS Code配置
    实现自行车接口示例和集合操作示例
    配置VS Code调试设置

---
+++ learn/src/main/java/cn/lixx/java/loop/Bicycle.java
@@ -0,0 +1,13 @@
+package cn.lixx.java.loop;
+
+interface Bicycle {
+
+    // wheel revolutions per minute
+    void changeCadence(int newValue);
+
+    void changeGear(int newValue);
+
+    void speedUp(int increment);
+
+    void applyBrakes(int decrement);
+}
---
+++ single-file-program/ReferenceNonJDKClass.java
@@ -0,0 +1,11 @@
+// import org.apache.commons.lang3.RandomUtils;
+
+// //wget https://dlcdn.apache.org//commons/lang/binaries/commons-lang3-3.18.0-bin.zip
+// //java -cp ./commons-lang3-3.18.0.jar ReferenceNonJDKClass.java 
+// public class ReferenceNonJDKClass {
+
+//     public static void main(String[] args) {
+//         System.out.println(RandomUtils.nextInt());
+//     }
+
+// }
