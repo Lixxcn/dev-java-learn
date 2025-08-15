540e6e317446d7f23cb20141558228ee771114ae
Lixx <i@lixx.cn>
2025-08-15 17:09:06 +0800

    feat: 初始化Java项目结构并添加基础示例代码
    添加Main.java和Demo.java作为基础示例
    配置pom.xml定义项目结构
    更新launch.json添加调试配置
    添加liaoxuefeng.txt作为学习笔记文档

---
+++ liaoxuefeng/src/main/java/cn/lixx/oop/Demo.java
@@ -0,0 +1,10 @@
+package cn.lixx.oop;
+
+public class Demo {
+    public static void main(String[] args) {
+        boolean b = "Hello".contains("ll");
+        String s = "Hello";
+        System.out.println(b);
+    }
+
+}
